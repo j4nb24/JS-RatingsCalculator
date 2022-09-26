f77bd0110c46224d2caeb1537f3e56a4919472f6
Jan <jan.banda@outlook.com>
2022-09-26 11:55:12 +0100

    v0.1

--- js/ratings.js
+++ js/ratings.js
@@ -12,9 +12,23 @@ function collect_ratings() {
 
     parseelements.forEach(element => {
         rating = parseInt(element.id.replace('star', ''));
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
     });
-    
+
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+
+    return ratings; 
 }
+
+document.addEventListener('change', () => {
+    const ratings = collect_ratings();
+    ratings.average.toFixed(2) = document.querySelector('average').value;
+})
+
+
 
 /* CREATE FUNCTION 
 const collect_ratings = () => {
